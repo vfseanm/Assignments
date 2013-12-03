b6ebdfc615ef6c1a59739c68d8a44f2568d8c3ef
vfseanm <vfseanm@gmail.com>
2013-12-03 18:39:08 -0500

    rackett

--- racket/basics.rkt
+++ racket/basics.rkt
@@ -1,22 +1,11 @@
 #lang racket
-
+(define (nth lst n) 
+  (cond
+    ((zero? n) (car lst))
+    (else
+      (nth (cdr lst) (- n 1)))))
 
 ;;; Return the sum of all numbers up to and including n for 0 <= n.
-<<<<<<< HEAD
-(define (sum-to-n n) 
-  (if (zero? n)
-    0
-    (+ n (sum-to-n (- n 1)))))
-
-;;; Calculate n choose r, for 0 <= n, 0 <= r <= n
-(define (combination n r) 
-  (cond 
-    ((zero? r) 1)
-    ((= n r) 1)
-    (else
-      (+ (combination (- n 1) r)
-         (combination (- n 1) (- r 1))))))
-=======
 (define (sum-to-n n)
   (cond
    ((zero? n) 0)
@@ -31,7 +20,6 @@
    (else
     (+ (combination (- n 1) (- r 1))
        (combination (- n 1) r)))))
->>>>>>> 778a4fa6f29a0a16c030e6224c82535bed5e8d9c
 
 (define (fib n) 
   (cond
@@ -43,19 +31,6 @@
 ;;; Return the sum of all numbers in a list
 (define (sum lst)
   (cond
-<<<<<<< HEAD
-    ((null? lst) 0)
-    (else
-      (+ (car lst) (sum (cdr lst))))))
-
-;;; Return whether element a is contained in list lst
-(define (member? a lst) 
-  (cond
-    ((null? lst) #f)
-    ((equal? a (car lst)) #t)
-    (else
-      (member? a (cdr lst)))))
-=======
    ((null? lst) 0)
    (else
     (+ (car lst) (sum (cdr lst))))))
@@ -67,25 +42,14 @@
    ((equal? a (car lst)) #t)
    (else
     (member? a (cdr lst)))))
->>>>>>> 778a4fa6f29a0a16c030e6224c82535bed5e8d9c
 
 ;;; Return a list with the *first* instance of element a removed if present
 (define (rember lst a)
   (cond
-<<<<<<< HEAD
-    ((null? lst) lst)
-    ((equal? a (car lst)) (cdr lst))
-    (else
-      ((cons (car lst) rember (cdr lst) a)))))
-
-(define (atom? a)
-  (not (list? a)))
-=======
    ((null? lst) '())
    ((equal? a (car lst)) (cdr lst))
    (else
     (cons (car lst) (rember (cdr lst) a)))))
->>>>>>> 778a4fa6f29a0a16c030e6224c82535bed5e8d9c
 
 ;;; Return whether element a is contained in any level of S-expression lst
 (define (member?* a lst) 
@@ -111,23 +75,21 @@
 
 ;;; Return whether the list contains two equal adjacent elements
 (define (two-in-a-row? lst) 
+  (cond
   ((null? lst) #f)
-  ((null? (cdr lst) #f))
-  ((equal? (car lst) (cadr lst))) #t)
+  ((null? (cdr lst)) #f)
+  ((equal? (car lst) (cadr lst)) #t)
   (else
-    (two-in-a-row? (cdr lst))))
-
-;;; Return the nth element of a list
-(define (nth lst n) null)
+    (two-in-a-row? (cdr lst)))))
 
 ;;; Return a list containing the unique elements of lst
 (define (dedup lst)
   (cond
     ((null? lst) null)
     ((member? (car lst) (cdr lst))
-      (dedup (cr lst)))
+      (dedup (cdr lst)))
     (else
-      (cons (car lst) (dedup (caar lst))))))
+      (cons (car lst) (dedup (cdr lst))))))
 
 ;;; Return a list containing the elements of lst in reverse order
 (define (reverse lst)
